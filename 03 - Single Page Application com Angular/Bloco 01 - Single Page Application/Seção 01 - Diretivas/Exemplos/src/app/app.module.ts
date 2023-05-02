f9043644923eb354e398f3419409445c168b1c2c
Tiago Lourinho <lourinho.tiago@gmail.com>
2023-05-02 17:15:39 -0300

    feat: directives

--- 03 - Single Page Application com Angular/Bloco 01 - Single Page Application/Seção 01 - Diretivas/Exemplos/src/app/app.module.ts
+++ 03 - Single Page Application com Angular/Bloco 01 - Single Page Application/Seção 01 - Diretivas/Exemplos/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+import { ArticleComponent } from './article/article.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    CardComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- 03 - Single Page Application com Angular/Bloco 01 - Single Page Application/Seção 01 - Diretivas/Exemplos/src/app/card/card.component.ts
+++ 03 - Single Page Application com Angular/Bloco 01 - Single Page Application/Seção 01 - Diretivas/Exemplos/src/app/card/card.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent {
+  title: string = "Card Component is working!";
+  showP: boolean = false;
+  products: string[] = ["Mouse", "Keyboard", "USB Cable", "Mouse Pad"];
+  inputProducts: string = "";
+  menuType: string = "admin";
 
+  add() {
+    this.products.push(this.inputProducts);
+    console.log(this.products);
+  }
+
+  remove(index: number) {
+    this.products.splice(index, 1);
+  }
+
+  enterKey(event: any) {
+    this.add();
+  }
 }
