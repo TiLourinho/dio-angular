61c02a09c3c427fad3da34ab55a2c09bde0b8c7a
Tiago Lourinho <lourinho.tiago@gmail.com>
2023-05-03 14:57:46 -0300

    feat: modules

--- 03 - Single Page Application com Angular/Bloco 02 - Módulos Angular/Seção 01 - Módulos/Exemplos/src/app/app.module.ts
+++ 03 - Single Page Application com Angular/Bloco 02 - Módulos Angular/Seção 01 - Módulos/Exemplos/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ShoppingListModule } from './shopping-list/shopping-list.module'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ShoppingListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
